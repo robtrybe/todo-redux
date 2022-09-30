6a2757ab33fa3f0ab48faf3d73c3bb3887899998
Felipe Pinto <felipe14062000@gmail.com>
2022-09-30 16:34:58 -0300

    Criação do component Tasks

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import getTasks from '../services/tasksApi'
+
+class Tasks extends Component {
+  state = { 
+    tasks: [],
+   };
+  componentDidMount() {
+    const tasks = getTasks()
+    this.setState({
+      tasks,
+    })
+  }
+
+  render() {
+    const { tasks } = this.state;
+    return(
+      <>
+      <ol>
+      { tasks.map((texto) => <li>{ texto }</li>) }
+      </ol>
+      </>
+    )
+  }
+}
