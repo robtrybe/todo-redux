6613c7345ff83b0a549ca8dafee0824811354a10
Robson Narcizo <rob.narcizo.27@gmail.com>
2022-10-18 12:16:14 -0300

    Criando Container de Tarefas

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import '../styles/Form.css';
 import { ImAddressBook } from 'react-icons/im';
+import { MdEditNote } from 'react-icons/md';
 
 class Form extends Component {
   render() {
@@ -9,7 +10,8 @@ class Form extends Component {
         <h1>Lista de Tarefas</h1>
         <form className="form-task content">
         <input type="text" name="task" placeholder='Adicione um tarefa' />
-        <button type="button"><ImAddressBook /></button> 
+        <button type="button"><ImAddressBook /></button>
+        <button type="button" className='edit'><MdEditNote /></button>
         </form>
       </>
     )
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import { TbEdit } from 'react-icons/tb';
+import { MdDeleteForever } from 'react-icons/md';
 
 class Tasks extends Component {
   render() {
     return (
-      <div className="tasks-container">Tasks</div>
+      <div className="tasks-container content">
+        <h2>Tarefas a realizar</h2>
+        <div className="task-container">
+          <div className="task">Tomar café</div>
+          <div className="btn-container">
+            <button type="button" className="edit"><TbEdit /></button>
+            <button type="button" className='delete'><MdDeleteForever /></button>
+          </div>
+        </div>
+        <div className="task-container">
+          <div className="no-task">Nenhum tarefa a realizar no momento</div>
+        </div>
+      </div>
     )
   }
 }
